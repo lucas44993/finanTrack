526507e53c5006f393401ea25d123e88484a6019
lucas44993 <lucasabraao2020@hotmail.com>
2024-09-19 16:17:23 -0300

    Criei a classe para a conexão do MesporContatro com o banco de dados

---
+++ src/main/java/com/finanTrack/domain/mêsPorContrato/MesPorContratoRepository.java
@@ -0,0 +1,6 @@
+package com.finanTrack.domain.mêsPorContrato;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface MesPorContratoRepository extends JpaRepositoryImplementation<MesPorContrato, Double> {
+}
