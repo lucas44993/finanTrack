1cfa60a7029b970e56fb9b29c164611c2f89cdeb
lucas44993 <lucasabraao2020@hotmail.com>
2024-09-26 19:19:36 -0300

    Refiz todo o projeto.

--- src/main/java/com/finanTrack/domain/contratos/ContratosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.finanTrack.domain.contratos;
-
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
-
-public interface ContratosRepository extends JpaRepositoryImplementation<Contratos,Integer> {
-
-}
